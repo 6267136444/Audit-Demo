a3b5ef3eacdbb8ec6b1c3e2c537a0f53e8fe65be
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2025-07-27 09:51:57 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Resource Group"
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure location"
+  default     = "centralindia"
+}
+
+variable "aks_cluster_name" {
+  type        = string
+  description = "The name of the AKS cluster"
+}
+
+variable "dns_prefix" {
+  type        = string
+  description = "DNS prefix for the AKS cluster"
+}
+
+variable "node_count" {
+  type        = number
+  default     = 1
+}
+
+variable "vm_size" {
+  type        = string
+  default     = "Standard_DS2_v2"
+}
