d26d7fee50c531465e60827bef2c8eb61afafac0
Shubham Patel <136552538+6267136444@users.noreply.github.com>
2025-07-27 21:47:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  use_oidc         = true
+  tenant_id        = var.tenant_id
+  subscription_id  = var.subscription_id
+  client_id        = var.client_id
 }
 
 resource "azurerm_resource_group" "aks_rg" {
